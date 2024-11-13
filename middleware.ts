09b28b0e20a1c3536dec307032f169335f2045d3
Diipz <dip_patel100@hotmail.com>
2024-11-13 19:58:54 +0000

    Attmept to implement Arcjet middleware with Clerk

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,36 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import arcjet, { fixedWindow, createMiddleware } from "@arcjet/next";
+import { NextResponse } from "next/server";
 
 // import { clerkMiddleware } from "@clerk/nextjs/server";
 
 
-// Initialize Arcjet middleware with its configuration
-const aj = arcjet({
+// const arcjetMware =  createMiddleware(aj);
+
+// Arcjet blocks webhooks by default which should be excluded as below
+const isProtectedRoute = createRouteMatcher(["/((?!webhook).*)"]);
+
+const clerk = clerkMiddleware(async (auth, req) => {
+  // May add additional protected routes
+  if (isProtectedRoute(req)) {
+    await auth.protect()
+  }
+  return NextResponse.next();
+})
+
+ // Initialize Arcjet middleware with its configuration
+ const aj = arcjet({
   key: process.env.ARCJET_KEY!,
   rules: [
     fixedWindow({
       mode: "LIVE",
       window: "1h",
-      max: 200,
+      max: 10,
     }),
   ],
 });
 
-const arcjetMware =  createMiddleware(aj);
-
-// Arcjet blocks webhooks by default which should be excluded as below
-const isProtectedRoute = createRouteMatcher(["/((?!webhook).*)"]);
-
-export default clerkMiddleware(async (auth, req, event) => {
-  // May add additional protected routes
-  if (isProtectedRoute(req)) await auth.protect()
-
-  return arcjetMware(req, event);
-})
+export default createMiddleware(aj, clerk);
 
 // export default clerkMiddleware();
 
