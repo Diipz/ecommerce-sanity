003a5e00ce7334161023ade73aa779a832ce1dbb
Diipz <dip_patel100@hotmail.com>
2024-11-14 13:22:10 +0000

    Attempt to implement Arcjet

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,33 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import arcjet, { fixedWindow } from "@arcjet/next";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
+
+// Initialize Arcjet middleware with its configuration
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    fixedWindow({
+      mode: "LIVE",
+      window: "1h",
+      max: 10,
+    }),
+  ],
+});
+
+
+const isProtectedRoute = createRouteMatcher(["/((?!webhook).*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // May add additional protected routes
+  if (isProtectedRoute(req)) await auth.protect();
+
+  aj.protect(req);
+  
+
+  return NextResponse.next();
+})
 
 export const config = {
   matcher: [
